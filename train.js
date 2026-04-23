287fcfc232c90dec10b0d579b28177547b1b8fb4
Dina-me10 <dinaisakova10@gmail.com>
2026-04-24 01:48:29 +0900

    feat: MIT task 1

--- train.js
+++ train.js
@@ -1,41 +1,58 @@
-console.log("Jack Ma maslahatlari");
+//MIT TASK 1
+function countLetter(letter, word) {
+  let count = 0;
 
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60+
-];
+  const lowerLetter = letter.toLowerCase();
+  const lowerWord = word.toLowerCase();
+
+  for (let i = 0; i < lowerWord.length; i++) {
+    if (lowerWord[i] === lowerLetter) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+const result = countLetter("a", "Madina");
+console.log(result);
+
+//const list = [
+// "yahshi talaba boling", // 0-20
+// "togri boshliq tanlang va koproq hato qiling", // 20-30
+// "uzingizga ishlashingizni boshlang", // 30-40
+// "siz kuchli bolgan narsalarni qiling", // 40-50
+// "yoshlarga investitsiya qiling", // 50-60
+// "endi dam oling, foydasi yoq endi", // 60+
+//];
 
 //CALBACK FUNCTIONS
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number") callback("insert a number", null);
-  else if (a <= 20) callback(null, list[0]);
-  else if (a > 20 && a <= 30) callback(null, list[1]);
-  else if (a > 30 && a <= 40) callback(null, list[2]);
-  else if (a > 40 && a <= 50) callback(null, list[3]);
-  else if (a > 50 && a <= 60) callback(null, list[4]);
-  else {
-    // Asynchronous implementation using setInterval
-    setInterval(function () {
-      callback(null, list[5]);
-    }, 1000);
-  }
-}
+//function maslahatBering(a, callback) {
+// if (typeof a !== "number") callback("insert a number", null);
+// else if (a <= 20) callback(null, list[0]);
+// else if (a > 20 && a <= 30) callback(null, list[1]);
+// else if (a > 30 && a <= 40) callback(null, list[2]);
+//  else if (a > 40 && a <= 50) callback(null, list[3]);
+// else if (a > 50 && a <= 60) callback(null, list[4]);
+// else {
+// Asynchronous implementation using setInterval
+//   setInterval(function () {
+//     callback(null, list[5]);
+//   }, 1000);
+// }
+//}
 
-console.log("passed here 0");
+//console.log("passed here 0");
 
-maslahatBering(70, (err, data) => {
-  if (err) console.log("ERROR:", err);
-  else {
-    console.log("javob:", data);
-  }
-});
+//maslahatBering(70, (err, data) => {
+//  if (err) console.log("ERROR:", err);
+//  else {
+//   console.log("javob:", data);
+//  }
+//});
 
-console.log("passed here 1");
+//nsole.log("passed here 1");
 
 //ASYNC FUNCIONS
 // async function maslahatBering(a) {
